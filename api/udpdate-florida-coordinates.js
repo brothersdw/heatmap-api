49f192f6cfb9a36791d7cc4d7ca39ef593d2492a
dbrothers <dbrothers@ruvos.com>
2023-12-20 15:47:53 -0500

    Commented code

--- api/udpdate-florida-coordinates.js
+++ api/udpdate-florida-coordinates.js
@@ -1,29 +1,28 @@
-const axios = require("axios");
-const uuid = require("uuid").v4;
-const apikey = require("../keys/openCageDataKeys.json").apiKey;
-const fs = require("fs");
-const { stringify } = require("querystring");
-const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+const axios = require("axios"); // Import axios for sending request
+const uuid = require("uuid").v4; // Import uuid to create unique ids
+const fs = require("fs"); // Import fs for writing file
+
+// Async function to update county-boundaries.json file
 const updateCoordinates = async (req, res) => {
   try {
+    // Await GET request to opendatasoft to retrieve county boundaries
     const response = await axios.get(
       "https://public.opendatasoft.com//api/explore/v2.1/catalog/datasets/us-county-boundaries/records?select=geo_shape%2C%20namelsad&where=stusab%20%3D%20'FL'&limit=67"
     );
-    const coordinates = [];
-    console.log("Made it here", response);
-    await response.data.results.forEach((result, idx) => {
-      const countyBoundary = result.geo_shape;
-      let countyId = uuid();
-      let idExists = coordinates.filter((ids) => ids.id === countyId);
+    const coordinates = []; // Array to store coordinates
+
+    response.data.results.forEach((result, idx) => {
+      let countyId = uuid(); // Create unique county id
+      let idExists = coordinates.filter((ids) => ids.id === countyId); // Filter coordinates array to see if UUID exists
+
+      // While idExists array length is more than 0 create a new UUID, check again and log that there was an existing id
       while (idExists.length > 0) {
         countyId = uuid();
         idExists = coordinates.filter((ids) => ids.id === countyId);
         console.log(`There was an existing id ${countyId}`);
       }
-      console.log(
-        "coordinates: ",
-        result.geo_shape.geometry.coordinates.length > 1
-      );
+
+      // Create object for coordinates and push to coordinates array
       coordinates.push({
         id: countyId,
         county: result.namelsad,
@@ -31,24 +30,33 @@ const updateCoordinates = async (req, res) => {
         type: result.geo_shape.geometry.type,
       });
     });
-    coordinatesString = JSON.stringify(coordinates);
-    const jsonFilePath = "./data/county-boundaries.json";
-    console.log("Your stringified coordinates: ", coordinates);
+    coordinatesString = JSON.stringify(coordinates); // Stringify coordinates
+    const jsonFilePath = "./data/county-boundaries.json"; // File path to write to
+
+    // Write file to specified path
     fs.writeFile(jsonFilePath, JSON.stringify(coordinates), (err) => {
+      // If there is an error log message with file path and error
       if (err) {
         console.log(
           `There was an issue writing the JSON file for coordinates to ${jsonFilePath}: ${err}`
         );
+
+        // Else log success message with specified path
       } else {
         console.log(`JSON file successfully written to ${jsonFilePath}`);
       }
     });
+
+    // Send a 200 OK response with a success message
     res.status(200).send({
       message: "Sucessfully got coordinates",
     });
+    // If any error should occur log and send failure message along with error
   } catch (e) {
-    console.log(`Error fetching coordinates: ${e}`);
-    return res.status(500).send("There was a problem fetching coordinates");
+    console.log("Error fetching coordinates: ", e);
+    return res
+      .status(500)
+      .send(`There was a problem fetching coordinates ${e}`);
   }
 };
 
--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require("express");
-const axios = require("axios");
 const app = express();
-const port = 3008;
-const routes = require("./routes");
-const cors = require("cors");
+const port = 3008; // Set port
+const routes = require("./routes"); // Import all routes
+const cors = require("cors"); // !IMPORTANT - Import cors. This is so that you are able to use api from frontend
 
 const corsOptions = {
   origin: "http://localhost:3006", // Replace with your frontend's origin
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: false, // Enable credentials (cookies, authorization headers, etc.)
 };
-app.use(cors(corsOptions));
-app.use("/", routes);
+app.use(cors(corsOptions)); // Add cors with cors configs to app
+app.use("/", routes); // Add routes to app
+
+// Bring up app
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
