e582b11a8b85b029f1fb3c0afc39096b0c922672
dbrothers <dbrothers@ruvos.com>
2024-01-10 14:45:52 -0500

    Updated code to pull data from database and recreated mapbox data for state and county

--- api/get-state-mapbox-data.js
+++ api/get-state-mapbox-data.js
@@ -1,19 +1,60 @@
 const stateCoordinates = require("../data/state-boundaries.json"); // Import data from county-boundaries.json file
 const diseaseModel = require("../models/diseases"); // Import diseases model
 const randomNum = () => Math.floor(Math.random() * (200000 - 0 + 1)) + 0; // to simulate counts from Integration
+const countyCaseCountsModel = require("../models/county_case_counts");
 
 // Async function that builds data in the structure that mapbox expects
 const buildStateMapBoxData = async (req, res) => {
   try {
     const diseases = await diseaseModel.getDiseases(); // Await database query for diseases
+    const stateLevelCaseCounts = await countyCaseCountsModel
+      .getCountyCaseCounts()
+      .then((result) => {
+        const disease_keys = [];
+        result.map((r) =>
+          JSON.parse(r.incidences).map((i) => {
+            let keyExists = false;
+            let setIdx;
+
+            disease_keys.map((dk, idx) => {
+              if (Object.keys(i)[0] === Object.keys(dk)[0]) {
+                keyExists = true;
+                setIdx = `${idx}`;
+              }
+            });
+
+            if (!keyExists) {
+              return disease_keys.push({
+                [Object.keys(i)]: Object.values(i)[0],
+              });
+            } else {
+              disease_keys[setIdx][Object.keys(i)[0]] =
+                disease_keys[setIdx][Object.keys(i)[0]] + Object.values(i)[0];
+            }
+          })
+        );
+        return disease_keys;
+      })
+      .catch((err) => {
+        console.log(
+          "Something went wrong attempting to gather state leve case counts in get-state-mapbox-data.js:",
+          err
+        );
+      });
+
     const features = stateCoordinates.map((s) => {
       // For each set of coordinate arrays grab value of disease cases key for key, add random number as value
       // and add county to properties object
       const properties = diseases.reduce(
         (arr, field) => ({
           ...arr,
-          [field.disease_cases_key]: randomNum(),
-          county: s.name,
+          [field.disease_cases_key]: Object.values(
+            stateLevelCaseCounts.filter(
+              (s) => Object.keys(s)[0] === field.disease_cases_key
+            )[0]
+          )[0],
+          state: s.name,
+          isState: true,
         }),
         {}
       );
